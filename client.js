6d59bdec285387240a8d98a639c9ff3817df9a6c
C-rostad <Carter@rostad.ca>
2025-06-14 11:06:24 -0700

    experimented with movement for snake

--- client.js
+++ client.js
@@ -13,6 +13,9 @@ const connect = function() {
     console.log("Connected - Welcome to Snake!");
     conn.write("Name: CPR");
 
+
+   // setInterval(() =>  conn.write("Move: up"), 50);
+
   });
   conn.on("data", (data) => {
     console.log(data);
@@ -22,3 +25,12 @@ const connect = function() {
 };
 
 module.exports = connect;
+
+
+
+// Note
+
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
