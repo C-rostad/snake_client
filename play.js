b275e2fbb214c2161023578de0cbd2de3bcbc059
C-rostad <Carter@rostad.ca>
2025-06-14 10:54:21 -0700

    moved connect function to client file

--- play.js
+++ play.js
@@ -1,24 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("Connected - Welcome to Snake!");
-  });
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
+const connect = require("./client.js");
 
 console.log("Connecting ...");
 connect();
